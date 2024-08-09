a9c5fc04716fb7563a9d21c046af0577012fe309
Nikolas Garofil <nikolas@garofil.be>
2024-08-09 18:39:09 +0200

    script done

--- empty/build-static-ssh.sh
+++ empty/build-static-ssh.sh
@@ -14,27 +14,29 @@ ZLIB_DIR="zlib-${ZLIB_VERSION}"
 ZLIB_TGZ="$ZLIB_DIR.tar.gz"
 ZLIB_URL="https://zlib.net/${ZLIB_TGZ}"
 ZLIB_CHECKFILE="lib/libz.a"
-ZLIB_BUILD_COMMANDS="./configure --prefix=\"$root\" --static && make && make install"   # Build in static mode so that we can link statically with OpenSSH
+#Make sure it ends up in $root/lib(64), --static is needed because zlib doesn't like that you build static if you don't mention it before
+ZLIB_BUILD_COMMANDS="./configure --prefix=\"$root\" --static && make && make install"
 
 OPENSSL_DIR="openssl-${OPENSSL_VERSION}"
 OPENSSL_TGZ="$OPENSSL_DIR.tar.gz"
 OPENSSL_URL="https://www.openssl.org/source/${OPENSSL_TGZ}"
 OPENSSL_CHECKFILE="bin/openssl"
-OPENSSL_BUILD_COMMANDS="./config --prefix=\"$root\" no-shared no-tests && make && make install"  # Build in static mode so that we can link statically with OpenSSH
+OPENSSL_BUILD_COMMANDS="./config --prefix=\"$root\" no-tests && make && make install" #Make sure it ends up in $root/lib(64) and don't waste time with tests
 
 OPENSSH_DIR="openssh-portable-${OPENSSH_VERSION}"
 OPENSSH_TGZ="$OPENSSH_DIR.tar.gz"
 OPENSSH_URL="https://github.com/openssh/openssh-portable/archive/refs/tags/${OPENSSH_VERSION}.tar.gz"
 OPENSSH_CHECKFILE="bin/ssh"
-OPENSSH_BUILD_COMMANDS="autoreconf && LIBS=\"-lpthread\" ./configure --prefix=\"$root\" --exec-prefix=\"$root\" --with-privsep-user=nobody --with-ssl-dir=\"$root\" && make && make install"
+#Make sure it ends up in $root/bin, that it drops privileges and that it should use the OpenSSL instead of the one that comes with the ssh source code
+OPENSSH_BUILD_COMMANDS="autoreconf && ./configure --prefix=\"$root\" --exec-prefix=\"$root\" --with-privsep-user=nobody --with-ssl-dir=\"$root\" && make && make install"
 
-echo "We will be working in $top, things might get messy here. Press Ctrl+C to cancel now or Enter to continue"; read -r
-
-export "CPPFLAGS=-I$root/include -L. -fPIC"; export "CFLAGS=$CPPFLAGS" # Compiler will look for headers in $root/include, libraries in the current directory, generate position-independent code and link statically
-export "LDFLAGS=-L$root/lib -L$root/lib64 -static" # Compiler will look for libraries in $root/lib and $root/lib64
+read -p "We will be working in $top, things might get messy (t)here. Press Ctrl+C to cancel now or Enter to continue" ignorethisvariable
 
 set -uex    # Show each command before executing it and exits when a command returns a non-zero exit code or a variable is used without being set
 umask 0077  # Make sure that no one except the owner can read, write, or execute newly created files
+
+export "CPPFLAGS=-I$root/include -L. -fPIC -pthread"; export "CFLAGS=$CPPFLAGS" # Compiler will look for headers in $root/include, libraries in the current directory and generate position-independent code
+export "LDFLAGS=-L$root/lib -L$root/lib64 -static" # Linker will look for libraries in $root/lib and $root/lib64 and link statically
 
 #Check if everything needed is available
 autoreconf --version || { echo "You still need to install autoconf"; exit 1; }
